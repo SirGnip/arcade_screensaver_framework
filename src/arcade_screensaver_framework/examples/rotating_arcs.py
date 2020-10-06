4835dc35c5ff1c1baaf0e57b0102cf7f82ca0347
SirGnip <SirGnip@users.noreply.github.com>
2020-10-05 22:15:01 -0500

    rotating_arcs: Use lights to make background

--- src/arcade_screensaver_framework/examples/rotating_arcs.py
+++ src/arcade_screensaver_framework/examples/rotating_arcs.py
@@ -3,6 +3,7 @@ import random
 from dataclasses import dataclass
 import arcade
 from gnp.arcadelib.actor import Actor, ActorList
+from arcade.experimental.lights import Light, LightLayer
 from arcade_screensaver_framework import screensaver_framework
 from arcade_curtains import Curtains, BaseScene, KeyFrame, Sequence
 from arcade_curtains.animation import AnimationManagerProxy
@@ -127,6 +128,20 @@ class SingleScene(BaseScene):
     def setup(self, win):
         left, self.screen_width, bottom, self.screen_height = win.get_viewport()
         self.coils = ActorList()
+        self.light_layer = LightLayer(self.screen_width, self.screen_height)
+        # self.light_layer.set_background_color(arcade.color.BLACK)
+        radius = 1000
+        b = arcade.color.MIDNIGHT_BLUE
+        darken = 0.2
+        light_color = b[0] * darken, b[1] * darken, b[2] * darken
+        self.light1 = Light(0, 0, radius=radius, mode='soft', color=light_color)
+        self.light_layer.add(self.light1)
+        self.light2 = Light(0, 0, radius=radius, mode='soft', color=light_color)
+        self.light_layer.add(self.light2)
+        self.light3 = Light(0, 0, radius=radius, mode='soft', color=light_color)
+        self.light_layer.add(self.light3)
+        self.light4 = Light(0, 0, radius=radius, mode='soft', color=light_color)
+        self.light_layer.add(self.light4)
 
     def add_coil(self):
         x = random.randint(0, self.screen_width)
@@ -139,11 +154,29 @@ class SingleScene(BaseScene):
     def enter_scene(self, previous_scene):
         for _ in range(COIL_COUNT):
             self.add_coil()
+        self.add_anim_to_light(self.light1)
+        self.add_anim_to_light(self.light2)
+        self.add_anim_to_light(self.light3)
+        self.add_anim_to_light(self.light4)
+
+    def add_anim_to_light(self, light):
+        light.animate = AnimationManagerProxy(light)
+        elapsed = 0.0
+        seq = Sequence(loop=True)
+        for _ in range(10):
+            key = KeyFrame(
+                position=(random.randint(0, self.screen_width), random.randint(0, self.screen_height))
+            )
+            seq.add_keyframe(elapsed, key)
+            elapsed += random.uniform(5.0, 20.0)
+        light.animate(seq)
 
     def draw(self):
+        with self.light_layer:
+            arcade.draw_xywh_rectangle_filled(0, 0, self.screen_width, self.screen_height, arcade.color.WHITE)
+        self.light_layer.draw(ambient_color=arcade.color.BLACK)
         self.coils.draw()
         self.coils.update(0.0)  # to make sure coils get reaped from ActorList
-        time.sleep(0.02)
 
 
 class RotatingArcsSaver(arcade.Window):
