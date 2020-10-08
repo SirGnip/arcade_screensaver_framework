13cc7e0d18762671af60c0de9056084249b95803
SirGnip <SirGnip@users.noreply.github.com>
2020-10-07 20:07:48 -0500

    minimal_saver: make example even simpler

--- src/arcade_screensaver_framework/examples/minimal_saver.py
+++ src/arcade_screensaver_framework/examples/minimal_saver.py
@@ -1,4 +1,3 @@
-import time
 import arcade
 from arcade_screensaver_framework import screensaver_framework
 
@@ -6,25 +5,12 @@ from arcade_screensaver_framework import screensaver_framework
 class MinimalSaver(arcade.Window):
     def __init__(self, fullscreen, screen):
         super().__init__(fullscreen=fullscreen, screen=screen)
-        left, self.screen_width, bottom, self.screen_height = self.get_viewport()
         self.x = 0
-        self.xvel = 15
-
-    def update(self, delta):
-        self.x += self.xvel
-        if self.x < 0 or self.x > self.screen_width:
-            self.xvel *= -1
-        time.sleep(0.03)
 
     def on_draw(self):
+        self.x = (self.x + 10) % self.get_size()[0]
         arcade.start_render()
-        ctr_x = self.screen_width // 2
-        ctr_y = self.screen_height // 2
-
-        arcade.draw_rectangle_outline(self.x, ctr_y, 200, 200, arcade.color.DARK_RASPBERRY, border_width=15)
-        arcade.draw_text("Minimal Screen Saver",
-                         ctr_x, ctr_y,
-                         arcade.color.WHITE, 36, anchor_x="center")
+        arcade.draw_rectangle_filled(self.x, self.get_size()[1] / 2, 200, 200, arcade.color.BLUE)
 
 
 if __name__ == "__main__":
